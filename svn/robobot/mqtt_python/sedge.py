f609b78d412cdf2b32833eb604953c29eb41cc7e
Vito Antonio Vultaggio <87416639+huskyVV@users.noreply.github.com>
2026-03-14 20:16:59 +0100

    Update sedge.py

--- svn/robobot/mqtt_python/sedge.py
+++ svn/robobot/mqtt_python/sedge.py
@@ -69,8 +69,8 @@ class SEdge:
     lineCtrl = False # private
     # try with a P-Lead controller
     lineKp = 1.0 # 5  (rad/s per sensor value)
-    lineTauZ = 0.8 # 0.8 (second)
-    lineTauP = 0.25 # 0.15 (second)
+    lineTauZ = 1 # 0.8 (second)
+    lineTauP = 0.23 # 0.15 (second)
     # Lead pre-calculated factors
     tauP2pT = 1.0
     tauP2mT = 0.0
